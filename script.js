da8ab5e17bbfde867c0f8d85d966a66a81c91833
Krishnam02 <121663431+Krishnam02@users.noreply.github.com>
2025-08-13 21:35:36 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+AOS.init({
+    duration: 1000,
+    once: true
+});
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
